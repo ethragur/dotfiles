43a551fb222de2333e79a7a08af1d499ca93bfe2
ethragur <stefan.kuhnert94@gmail.com>
2015-09-30 15:01:16 +0200

    Updates

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,7 @@ alias ls 'ls --color=auto'
 alias nani 'livestreamer http://www.twitch.tv/naniwasc2 high'
 alias ll 'ls -l --color=auto'
 alias la 'ls -l -a --color=auto'
-alias inst 'sudo pacman -S'
+alias inst 'sudo yaourt -S'
 alias uinst 'sudo pacman -Rsnc'
 alias i3conf 'vim ~/.i3/config'
 alias battlenet 'wine ~/.wine/drive_c/Program\ Files\ \(x86\)/Battle.net/Battle.net\ Launcher.exe'
@@ -26,6 +26,8 @@ eval (dircolors -c ~/.dircolors | sed 's/>&\/dev\/null$//')
 
 set LD_PRELOAD "libpthread.so.0 libGL.so.1"
 set __GL_THREADED_OPTIMIZATIONS 1
+
+fish_vi_mode
 
 
 function sudo
