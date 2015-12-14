4e7019cf778f7bcd3b1c5b4de464f1168f082eb9
ethragur <stefan.kuhnert94@gmail.com>
2015-12-14 21:46:09 +0100

    Changed updatescript

--- updateDotfiles.sh
+++ updateDotfiles.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-ln -sf /home/effi/dotfiles/vim/.vimrc ~/.vimrc
 ln -sf /home/effi/dotfiles/i3/config ~/.i3/config 
-ln -sf /home/effi/dotfiles/i3/i3blocks.conf ~/.i3blocks.conf
-sudo ln -sf /home/effi/dotfiles/i3/i3status.conf /etc/i3status.conf
 ln -sf /home/effi/dotfiles/ranger/rc.conf /home/effi/.config/ranger/rc.conf
-ln -sf /home/effi/dotfiles/urxvt/Xresources /home/effi/.Xresources
 ln -sf /home/effi/dotfiles/urxvt/Xdefaults /home/effi/.Xdefaults
 ln -sf /home/effi/dotfiles/fish/config.fish /home/effi/.config/fish/config.fish
 ln -sf /home/effi/dotfiles/vim /home/effi/.config/nvim
-ln -sf /home/effi/dotfiles/.bashrc /home/effi/.bashrc
 ln -sf /home/effi/dotfiles/.xinitrc /home/effi/.xinitrc
 ln -sf /home/effi/dotfiles/termite/config /home/effi/.config/termite/config
 
