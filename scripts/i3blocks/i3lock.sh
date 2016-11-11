78957495a68549035140703c6237ac55b27f8932
Effi <stefan.kuhnert94@gmail.com>
2016-11-11 12:22:42 +0100

    Removed vsync from Compton, new rofi configs replaces dmenu, new/faster i3lock script

--- scripts/i3blocks/i3lock.sh
+++
@@ -1,9 +0,0 @@
-echo -e " \n\n$color3\n"
-
-if [ "$BLOCK_BUTTON" = "1" ]
-then 
-	scrot /tmp/screenshot.bmp
-	convert /tmp/screenshot.bmp -resize 10% /tmp/screenshotnoise.png
-	convert /tmp/screenshotnoise.png -blur 0x1 -resize 1000% /tmp/screenshotblur.png
-	i3lock -i /tmp/screenshotblur.png
-fi
