2647865086320008026feb3de15fc28591ea3567
Stefan Kuhnert <stefan.kuhnert94@gmail.com>
2020-01-04 08:41:42 +0100

    [ i3, picom, pywal ]

--- zsh/environment.zsh
+++ zsh/environment.zsh
@@ -1,4 +1,4 @@
-export PATH="/usr/bin/:/usr/local/bin/:/usr/local/sbin:/usr/sbin/:/home/effi/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/bin/:/home/effi/go/bin/"
+export PATH="/usr/bin/:/usr/local/bin/:/usr/local/sbin:/usr/sbin/:/home/effi/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/bin/:/home/effi/go/bin/:$HOME/.local/bin/"
 export JAVA_HOME="/usr/lib/jvm/openjdk/"
 export LANG=en_US.UTF-8
 export ARCHFLAGS="-arch x86_64"
@@ -14,20 +14,3 @@ export GPG_TTY=$(tty)
 export GTK_IM_MODULE=fcitx
 export QT_IM_MODULE=fcitx
 export XMODIFIERS="@im=fcitx"
-
-export color0="#282828"
-export color8="#cc241d"
-export color1="#98971a"
-export color9="#d79921"
-export color2="#458588"
-export color10="#b16286"
-export color3="#689d6a"
-export color11="#a89984"
-export color4="#928374"
-export color12="#fb4934"
-export color5="#b8bb26"
-export color13="#fabd2f"
-export color6="#83a598"
-export color14="#d3869b"
-export color7="#8ec07c"
-export color15="#ebdbb2"
